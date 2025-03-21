af5920adf1f04aa3a2a78ae4f6e1ee8a0a7225d4
Thomas Broyer <t.broyer@ltgt.net>
2025-03-21 18:16:07 +0100

    Fix functional tests in CI
    Use a volume rather than a bind-mount for persisting
    the keycloak data (mainly for local use for development)
    
    Also make the tests fail rather being ignored when
    they can't contact Keycloak.

--- common/src/testFixtures/java/net/ltgt/oauth/common/fixtures/Helpers.java
+++ common/src/testFixtures/java/net/ltgt/oauth/common/fixtures/Helpers.java
@@ -1,13 +1,13 @@
 package net.ltgt.oauth.common.fixtures;
 
 import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import com.nimbusds.oauth2.sdk.GeneralException;
 import com.nimbusds.oauth2.sdk.as.AuthorizationServerMetadata;
 import com.nimbusds.oauth2.sdk.as.ReadOnlyAuthorizationServerMetadata;
 import com.nimbusds.oauth2.sdk.id.Issuer;
 import java.io.IOException;
-import org.opentest4j.TestAbortedException;
 
 public class Helpers {
 
@@ -16,7 +16,7 @@ public class Helpers {
     try {
       return AuthorizationServerMetadata.resolve(new Issuer(issuer));
     } catch (GeneralException | IOException e) {
-      throw new TestAbortedException(
+      return fail(
           "Can't load authorization server metadata. Is Keycloak started and configured?", e);
     }
   }
